5c29e60d9ade64306336152e1f0b3188bd7e2f07
Codekacode <karem.quiroz.m@gmail.com>
2022-08-05 22:08:05 -0500

    Add footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
